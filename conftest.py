c5fcb7f52956d41097bca0972e4e1a37f80c0449
Dandev0 <99640203+Dandev0@users.noreply.github.com>
2022-08-01 02:22:30 +0500

    Delete conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture(scope="session")
-def browser():
-    driver = webdriver.Chrome(executable_path='C:\\Users\Danil\PycharmProjects\page_object\chromedriver_win32/chromedriver.exe')
-    yield driver
-    # driver.quit()
